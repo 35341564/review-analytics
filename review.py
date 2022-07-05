0338768ddad3518ac137033e8155d107dc6b976e
David Lee <david98djb@gmail.com>
2022-07-05 10:45:14 +0800

    update

--- review.py
+++ review.py
@@ -1,8 +1,14 @@
 data  = []
 count = 0
+i     = 0
 with open('reviews.txt','r')as f:
     for line in f:
         data.append(line)
         count += 1
         if count % 1000 == 0:
-            print(len(data))
+            for word_count in f:
+                data.append(word_count)
+                i = i + len(word_count)
+avg  = float( i / len(data))
+print('檔案讀取完畢，總共有',len(data),"筆資料")
+print("留言平均長度為:",avg)
